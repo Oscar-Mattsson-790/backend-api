12be94ee1f98b0d7ff8c043f9668b6d05b87b4f5
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-08-25 10:01:52 +0200

    Done

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Hello World!!!");
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/user");
+const blogRouter = require("./routes/blog");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use("/api/user", router);
+app.use("/api/blog", blogRouter);
+mongoose
+  .connect(
+    "mongodb+srv://oscarmattsson790:sodFPWun4QcUfuiD@cluster0.ntkl7ke.mongodb.net/Blog"
+  )
+  .then(() => app.listen(PORT))
+  .then(() => console.log(`Connected to the database on port ${PORT}...`));
